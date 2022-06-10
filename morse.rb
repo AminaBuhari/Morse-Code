e766ae8887bc178a491633de1ae2246675611331
Teshome Kurabachew <dear.teshe@gmail.com>
2022-06-10 21:35:14 +0000

    Add decode_char method

--- morse.rb
+++ morse.rb
@@ -1,3 +1,10 @@
+def decode_char(morse_code, decrypted, morse)
+  morse_code.each do |code|
+    code = code.split
+    decrypted.concat(code.map { |char| morse[char] }) << ' '
+  end
+end
+
 def decode_word(morse_code)
   morse = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' =>
     'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' =>
@@ -9,3 +16,5 @@ def decode_word(morse_code)
   decode_char(morse_code, decrypted, morse)
   decrypted.join
 end
+
+print decode_word('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
